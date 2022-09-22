dbfd54e93238d9c7027df325b49a84cb51caf8b7
John Aughey <jha@aughey.com>
2022-09-22 16:08:38 +0000

    seems to work first cut

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function PlayWave({ file, play }: { file: string, play: boolean }) {
+  const audioref = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (play) {
+      audioref.current?.play();
+    }
+  }, [play]);
+
+  return (<div>
+    <audio ref={audioref} src={file} preload="true" />
+  </div>);
+}
+
+function PlayCount({ count }: { count: number }) {
+  const numbers: { [key: number]: string } = {
+    5: "/five.wav",
+    4: "/four.wav",
+    3: "/three.wav",
+    2: "/two.wav",
+    1: "/one.wav"
+  };
+
+
+  return (
+    <div>
+      {Object.keys(numbers).map(n => parseInt(n)).map((n, i) => (<PlayWave file={numbers[n]} play={count === n} />))}
+    </div>
+  )
+
+}
+
+function RunAction({ action, whenComplete }: { action: number, whenComplete: () => void }) {
+  var [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let starttime = Date.now();
+
+    const Tick = () => {
+      var diff = Date.now() - starttime;
+      var seconds = (diff / 1000);
+      var countdown = action - seconds;
+      if (countdown < 0) {
+        stop()
+        whenComplete()
+        countdown = 0;
+      }
+      setTime(Math.ceil(countdown));
+    }
+
+    let interval = setInterval(Tick, 100);
+    const stop = () => clearInterval(interval);
+    return stop;
+  }, [action, whenComplete]);
+
+  return (<div>
+    <p>Running Action: {action}</p>
+    <div className="timer">{time}</div>
+    <PlayCount count={time} />
+  </div>)
+}
+
+function RunActions({ actions }: { actions: number[] }) {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    setIndex(0);
+  }, [actions]);
+
+  const WhenComplete = () => {
+    if (index < actions.length - 1) {
+      setIndex(index + 1);
+    }
+  }
+
+  return (<div>
+    <div>Actions: {JSON.stringify(actions)}, Index: {index}</div>
+    <RunAction action={actions[index] || 0} whenComplete={WhenComplete} />
+  </div>)
+}
+
 function App() {
+  var [code, setCode] = useState("6 5 4");
+  var [actions, setActions] = useState<number[]>([]);
+  var [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    if(running) {
+    setActions(code.split(" ").map((x) => parseInt(x)));
+    } else {
+      setActions([]);
+    }
+  }, [code, running])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={code} onChange={e => setCode(e.target.value)} type="text" />
+      <button onClick={() => setRunning(!running)}>{running ? "Stop Set" : "Run Set"}</button>
+      <RunActions actions={actions} />
     </div>
   );
 }
